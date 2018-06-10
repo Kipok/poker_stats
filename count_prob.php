6a3678434baae38d11781cc64641023c8d239e3a
Kipok <igor.a.gitman@gmail.com>
2018-06-10 06:42:42 +0000

    Add cmd line

--- count_prob.php
+++ count_prob.php
@@ -1,12 +1,16 @@
 <?php
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $num_ops = $_POST["num_ops"];
-        $cards_chosen = $_POST["cards_chosen"];
-        // TODO: validate number of cards
-       print_r($_POST);
-    } else {
-        http_response_code(403);
-        echo "There was a problem with your submission, please try again.";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $num_ops = $_POST["num_ops"];
+    $cards_chosen = $_POST["cards_chosen"];
+    // TODO: validate number of cards
+    $cmd = "";
+    foreach ($_POST as $key => $value) {
+        $cmd .= "--" . $key . '="' . (string)$value . '" ';
     }
+    echo $cmd;
+} else {
+    http_response_code(403);
+    echo "There was a problem with your submission, please try again.";
+}
 ?>
 

02c287e1444a949102598da71c587bf58e4991b5
Igor Gitman <ivypawn@gmail.com>
2018-10-14 13:05:02 +0700

    Add native parallel implementation

--- js/funcs.js
+++ js/funcs.js
@@ -1,150 +1,3 @@
-card2idx = {
-  "2c": 1,  "2d": 2,  "2h": 3,  "2s": 4,
-  "3c": 5,  "3d": 6,  "3h": 7,  "3s": 8,
-  "4c": 9,  "4d": 10, "4h": 11, "4s": 12,
-  "5c": 13, "5d": 14, "5h": 15, "5s": 16,
-  "6c": 17, "6d": 18, "6h": 19, "6s": 20,
-  "7c": 21, "7d": 22, "7h": 23, "7s": 24,
-  "8c": 25, "8d": 26, "8h": 27, "8s": 28,
-  "9c": 29, "9d": 30, "9h": 31, "9s": 32,
-  "Tc": 33, "Td": 34, "Th": 35, "Ts": 36,
-  "Jc": 37, "Jd": 38, "Jh": 39, "Js": 40,
-  "Qc": 41, "Qd": 42, "Qh": 43, "Qs": 44,
-  "Kc": 45, "Kd": 46, "Kh": 47, "Ks": 48,
-  "Ac": 49, "Ad": 50, "Ah": 51, "As": 52
-};
-
-idx2card = {};
-for (var key in card2idx) {
-    if (card2idx.hasOwnProperty(key)) {           
-        idx2card[card2idx[key]] = key;
-    }
-}
-
-function ids2cards(ids) {
-	cards = [];
-	for (var i = 0; i < ids.length; i++) {
-		cards[i] = idx2card[ids[i]];
-	}
-	return cards;
-}
-
-function shuffle(a) {
-    var j, x, i;
-    for (i = a.length - 1; i > 0; i--) {
-        j = Math.floor(Math.random() * (i + 1));
-        x = a[i];
-        a[i] = a[j];
-        a[j] = x;
-    }
-    return a;
-}
-
-function fill_cards(arr, num_cards, ids_left, ids_cnt) {
-    cur_arr = [];
-    var i = 0;
-    for (; i < arr.length; i++) {
-        cur_arr.push(card2idx[arr[i]]);
-    }
-    for (; i < num_cards; i++) {
-        cur_arr.push(ids_left[ids_cnt[0]++]);
-    }
-	return cur_arr;
-}
-
-function calc_prob(game_info, num_samples) {
-	var num_ops = parseInt(game_info["num_ops"]);
-    var hand = game_info["cards_you"];
-    var table = game_info["cards_table"];
-    var ids_used = [];
-    for (var key in game_info) {
-        if (!game_info.hasOwnProperty(key)) {
-            continue;
-        }
-        if (key === "num_ops")  {
-            continue;
-        }
-        for (var i = 0; i < game_info[key].length; i++) {
-            ids_used.push(card2idx[game_info[key][i]]);
-        }
-    }
-    ids_used.sort(function(a, b){return a - b});
-    var ids_left = [];
-    var j = 0;
-    for (var i = 1; i <= 52; i++) {
-        if (i === ids_used[j]) {
-            ++j;
-            continue;
-        }
-        ids_left.push(i);
-    }
-    var num_wins = 0;
-    var num_draws = 0;
-  
-    function map_fn(i) {
-       	console.log("!"); 
-		var ids_left_copy = ids_left.slice(0);
-		shuffle(ids_left_copy);
-        var ids_cnt = [0];
-        var cur_hand = fill_cards(hand, 2, ids_left_copy, ids_cnt);
-		var cur_table = fill_cards(table, 5, ids_left_copy, ids_cnt);
-		var your_hand = Hand.solve(ids2cards(cur_hand.concat(cur_table)));
-        var lost = 0;
-        var draw = 0;
-		for (var j = 1; j <= num_ops; j++) {
-			var cur_op = fill_cards(game_info["cards_op" + j], 2, ids_left_copy, ids_cnt);
-			var op_hand = Hand.solve(ids2cards(cur_op.concat(cur_table)));
-			var winners = Hand.winners([your_hand, op_hand]);
-            if (winners[0] !== your_hand) {
-                lost = 1;
-                break;
-            }
-            if (winners.length === 2) {
-                draw = 1;
-            }
-		}
-		if (lost === 1) {
-            return 0;
-        }
-        if (draw === 1) {
-            return 1;
-        } else {
-            return 2;
-        }
-    }; 
-  
-	function reduce_fn(d) {
-        for (var i = 0; i < num_samples; ++i) { 
-            if (d[i] === 0) {
-                continue;
-            }
-            if (d[i] === 1) {
-                ++num_draws;
-            } else {
-                ++num_wins;
-            }
-        }
-    };
-
-    var data = new Array(num_samples);
-    for (var i = 0; i < num_samples; ++i) {
-       data[i] = i;
-	}
-
-	if (false) {
-		var pool = new Parallel(data, maxWorkers=12);  
-		data.map(map_fn).then(reduce_fn);    
-	} else {
-		reduce_fn(data.map(map_fn));    
-	}
-
-    result_str = " Win: " + (100.0 * num_wins /  num_samples).toFixed(2) + "%<br>";
-    result_str += "Draw: " + (100.0 * num_draws /  num_samples).toFixed(2) + "%<br>";
-    result_str += "Lose: " + (100.0 - 100.0 * (num_draws + num_wins) /  num_samples).toFixed(2) + "%";
-    return result_str;
-}
-
-
 $(function() {
     var form_elem = $('#prob-form');
     var result_field = $("#result-prob");
@@ -177,8 +30,39 @@ $(function() {
         for (var i = 0; i < form_data.length; i++){
           game_info[form_data[i]['name']] = form_data[i]['value'];
         }
-		var prob_msg = calc_prob(game_info, game_info['num_samples']);
-        $(result_field).html(prob_msg);
+
+        var num_workers = 8;
+        var num_samples = game_info["num_samples"];
+        var promises = [];
+        for (var i = 0; i < num_workers; i++) {
+            promises.push(new Promise(function (fulfill, reject) {
+                var worker = new Worker('js/worker.js');
+                worker.onmessage = function(event) {
+                    fulfill(event.data);
+                };
+                worker.onerror = function(event) {
+                    reject(event.error);
+                };
+                worker.postMessage({game_info: game_info, num_samples: Math.floor(num_samples / num_workers)});
+            }));
+        }
+
+        Promise.all(promises)
+            .then(function(data) {
+                var num_wins = 0, num_draws = 0;
+                var actual_num_samples = Math.floor(num_samples / num_workers) * num_workers;
+                for (var i = 0; i < data.length; i++) {
+                    num_wins += data[i][0];
+                    num_draws += data[i][1];
+                }
+                prob_str = " Win: " + (100.0 * num_wins /  actual_num_samples).toFixed(2) + "%<br>";
+                prob_str += "Draw: " + (100.0 * num_draws /  actual_num_samples).toFixed(2) + "%<br>";
+                prob_str += "Lose: " + (100.0 - 100.0 * (num_draws + num_wins) /  actual_num_samples).toFixed(2) + "%";
+                $(result_field).html(prob_str);
+            })
+            .catch(function(error) {
+                console.log(error);
+            });        
     });
 });
 
---
+++ js/worker.js
@@ -0,0 +1,150 @@
+if( 'function' === typeof importScripts) {
+    self.importScripts("pokersolver.js");
+
+    card2idx = {
+        "2c": 1,  "2d": 2,  "2h": 3,  "2s": 4,
+        "3c": 5,  "3d": 6,  "3h": 7,  "3s": 8,
+        "4c": 9,  "4d": 10, "4h": 11, "4s": 12,
+        "5c": 13, "5d": 14, "5h": 15, "5s": 16,
+        "6c": 17, "6d": 18, "6h": 19, "6s": 20,
+        "7c": 21, "7d": 22, "7h": 23, "7s": 24,
+        "8c": 25, "8d": 26, "8h": 27, "8s": 28,
+        "9c": 29, "9d": 30, "9h": 31, "9s": 32,
+        "Tc": 33, "Td": 34, "Th": 35, "Ts": 36,
+        "Jc": 37, "Jd": 38, "Jh": 39, "Js": 40,
+        "Qc": 41, "Qd": 42, "Qh": 43, "Qs": 44,
+        "Kc": 45, "Kd": 46, "Kh": 47, "Ks": 48,
+        "Ac": 49, "Ad": 50, "Ah": 51, "As": 52
+    };
+
+    idx2card = {};
+    for (var key in card2idx) {
+        if (card2idx.hasOwnProperty(key)) {           
+            idx2card[card2idx[key]] = key;
+        }
+    }
+
+    function ids2cards(ids) {
+        cards = [];
+        for (var i = 0; i < ids.length; i++) {
+            cards[i] = idx2card[ids[i]];
+        }
+        return cards;
+    }
+
+    function shuffle(a) {
+        var j, x, i;
+        for (i = a.length - 1; i > 0; i--) {
+            j = Math.floor(Math.random() * (i + 1));
+            x = a[i];
+            a[i] = a[j];
+            a[j] = x;
+        }
+        return a;
+    }
+
+    function fill_cards(arr, num_cards, ids_left, ids_cnt) {
+        cur_arr = [];
+        var i = 0;
+        for (; i < arr.length; i++) {
+            cur_arr.push(card2idx[arr[i]]);
+        }
+        for (; i < num_cards; i++) {
+            cur_arr.push(ids_left[ids_cnt[0]++]);
+        }
+        return cur_arr;
+    }
+
+    function calc_prob(game_info, num_samples) {
+        var num_ops = parseInt(game_info["num_ops"]);
+        var hand = game_info["cards_you"];
+        var table = game_info["cards_table"];
+        var ids_used = [];
+        for (var key in game_info) {
+            if (!game_info.hasOwnProperty(key)) {
+                continue;
+            }
+            if (key === "num_ops") {
+                continue;
+            }
+            for (var i = 0; i < game_info[key].length; i++) {
+                ids_used.push(card2idx[game_info[key][i]]);
+            }
+        }
+        ids_used.sort(function(a, b){return a - b});
+        var ids_left = [];
+        var j = 0;
+        for (var i = 1; i <= 52; i++) {
+            if (i === ids_used[j]) {
+                ++j;
+                continue;
+            }
+            ids_left.push(i);
+        }
+        var num_wins = 0;
+        var num_draws = 0;
+      
+        function map_fn(i) {
+            var ids_left_copy = ids_left.slice(0);
+            shuffle(ids_left_copy);
+            var ids_cnt = [0];
+            var cur_hand = fill_cards(hand, 2, ids_left_copy, ids_cnt);
+            var cur_table = fill_cards(table, 5, ids_left_copy, ids_cnt);
+            var your_hand = Hand.solve(ids2cards(cur_hand.concat(cur_table)));
+            var lost = 0;
+            var draw = 0;
+             for (var j = 1; j <= num_ops; j++) {
+                 var cur_op = fill_cards(game_info["cards_op" + j], 2, ids_left_copy, ids_cnt);
+                 var op_hand = Hand.solve(ids2cards(cur_op.concat(cur_table)));
+                 var winners = Hand.winners([your_hand, op_hand]);
+                    if (winners[0] !== your_hand) {
+                        lost = 1;
+                        break;
+                    }
+                    if (winners.length === 2) {
+                        draw = 1;
+                    }
+             }
+             if (lost === 1) {
+                    return 0;
+                }
+                if (draw === 1) {
+                    return 1;
+                } else {
+                    return 2;
+                }
+            };
+
+        function reduce_fn(d) {
+            for (var i = 0; i < num_samples; ++i) { 
+                if (d[i] === 0) {
+                    continue;
+                }
+                if (d[i] === 1) {
+                    ++num_draws;
+                } else {
+                    ++num_wins;
+                }
+            }
+        };
+
+        var data = new Array(num_samples);
+        for (var i = 0; i < num_samples; ++i) {
+           data[i] = i;
+        }
+
+        reduce_fn(data.map(map_fn));    
+
+        return [num_wins, num_draws];
+
+
+     //    result_str = " Win: " + (100.0 * num_wins /  num_samples).toFixed(2) + "%<br>";
+     //    result_str += "Draw: " + (100.0 * num_draws /  num_samples).toFixed(2) + "%<br>";
+     //    result_str += "Lose: " + (100.0 - 100.0 * (num_draws + num_wins) /  num_samples).toFixed(2) + "%";
+     //    return result_str;
+    }
+
+    self.onmessage = function(event) {
+        self.postMessage(calc_prob(event.data.game_info, event.data.num_samples));
+    };
+}
